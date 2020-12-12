5ea1bb80bcb600d0ac5d638c99595b9780b4c0bd
jbazans <jbazans@uni.pe>
2020-12-12 11:08:58 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sistema para Colegios</title>
+	<link rel="stylesheet" type="text/css" href="css/index.css">
+</head>
+<body>
+	<div class="main-body">
+		<div class="navbar-content">
+			<div class="user-detail">
+				<img src="assets/img/user.jpg">
+				<h4 class="mt10">Jose Bazán</h4>
+				<center>
+					<h5 class="mt10">Programación</h5>
+				</center>
+			</div>
+			<div class="navbar-detail">
+				<ul>
+					<li>Inicio</li>
+					<li>Gestión alumnos</li>
+					<li>Salir</li>
+				</ul>
+			</div>
+		</div>
+		<div class="body-content">
+			<h2>Lista de alumnos</h2>
+			<table class="mt10">
+				<tbody>
+					<tr>
+						<th>Código</th>
+						<th>Nombre</th>
+						<th>Grado - Sección</th>
+					</tr>
+					<tr>
+						<td>1</td>
+						<td>Luis</td>
+						<td>3 - A</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+	</div>
+</body>
+</html>
